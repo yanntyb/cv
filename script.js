fb1c56e9b4762cbd614248f4735648b39cc874f3
yann tyb <yanntyb.lbc@gmail.com>
2021-09-29 15:36:28 +0200

    h2 anim

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-let title = document.querySelectorAll("h1, h2");
+let h1 = document.querySelectorAll("h1");
+let h2 = document.querySelectorAll("h2");
 
-for(let elem of title){
+
+for(let elem of h1){
     const fullName = elem.innerHTML;
     elem.innerHTML = elem.innerHTML[0];
     let index = 1;
@@ -13,4 +15,22 @@ for(let elem of title){
         interval();
     },100)
     interval();
+}
+
+for(let elem of h2){
+    const letterList = elem.innerHTML.split("");
+    elem.innerHTML = "";
+    let x = Math.random();
+    for(let letter of letterList){
+        let span = document.createElement("span");
+        span.innerHTML = letter;
+        elem.appendChild(span);
+        let interval = (pos) => window.setTimeout(() => {
+            span.style.fontSize = (Math.abs(Math.cos(pos) * 100)).toString() + "%";
+            pos += 0.005;
+            interval(pos);
+        },20);
+        interval(x);
+        x+=0.1;
+    }
 }
